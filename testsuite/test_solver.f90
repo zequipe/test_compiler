77a2d8daa042d4615071a90e7b09096956cf360c
Zaikun <zaikunzhang@gmail.com>
2021-12-12 19:11:15 +0800

    add more loops

--- testsuite/test_solver.f90
+++ testsuite/test_solver.f90
@@ -22,9 +22,11 @@ implicit none
 character(len=PNLEN) :: probname
 character(len=PNLEN) :: probs(100)
 integer(IK) :: iprob
+integer(IK) :: isol
 integer(IK) :: ir
 integer(IK) :: n
 integer(IK) :: nprobs
+integer(IK) :: nsols
 integer(IK) :: nr
 procedure(FUN), pointer :: calfun
 real(RP) :: Delta0
@@ -33,46 +35,49 @@ real(RP), allocatable :: x(:)
 type(problem_t) :: prob
 
 
+nsols = 3_IK
 nprobs = 3_IK
 probs(1:nprobs) = ['chebyqad', 'chrosen ', 'trigsabs']
 nr = 3_IK
 
-do iprob = 1, nprobs
-    probname = probs(iprob)
-    do ir = 1, nr
-        print *, 'Unconstrained problem No.', iprob, '        Random run No.', ir
-        n = (ir - 1_IK) * 10_IK + 1_IK
-        ! Construct the testing problem.
-        call construct(prob, probname, n)
+do isol = 1, nsols
+    do iprob = 1, nprobs
+        probname = probs(iprob)
+        do ir = 1, nr
+            print *, 'Solver No.', isol, '    Unconstrained problem No.', iprob, '    Random run No.', ir
+            n = (ir - 1_IK) * 10_IK + 1_IK
+            ! Construct the testing problem.
+            call construct(prob, probname, n)
 
-        ! Read X0.
-        call safealloc(x, prob % n) ! Not all compilers support automatic allocation yet, e.g., Absoft.
-        x = noisy(prob % x0)
+            ! Read X0.
+            call safealloc(x, prob % n) ! Not all compilers support automatic allocation yet, e.g., Absoft.
+            x = noisy(prob % x0)
 
-        ! Read objective/constraints.
-        orig_calfun => prob % calfun
-        calfun => noisy_calfun  ! Impose noise to the test problem
+            ! Read objective/constraints.
+            orig_calfun => prob % calfun
+            calfun => noisy_calfun  ! Impose noise to the test problem
 
-        ! Read other data.
-        Delta0 = prob % Delta0
+            ! Read other data.
+            Delta0 = prob % Delta0
 
-        ! Call the solver.
-        !call solver_unc(calfun, x, f, Delta0)  ! sunf95 cannot handle this
-        call solver_unc(noisy_calfun, x, f, Delta0)
+            ! Call the solver.
+            !call solver_unc(calfun, x, f, Delta0)  ! sunf95 cannot handle this
+            call solver_unc(noisy_calfun, x, f, Delta0)
 
-        ! Try modifying PROB.
-        prob % x0 = x
+            ! Try modifying PROB.
+            prob % x0 = x
 
-        ! Try outputting PROB.
-        print *, 'Unconstrained problem solved with X = ', prob % x0
-        print *, ''
+            ! Try outputting PROB.
+            print *, 'Unconstrained problem solved with X = ', prob % x0
+            print *, ''
 
-        ! Clean up.
-        ! Destruct the testing problem.
-        call destruct(prob)
-        deallocate (x)
-        nullify (calfun)
-        nullify (orig_calfun)
+            ! Clean up.
+            ! Destruct the testing problem.
+            call destruct(prob)
+            deallocate (x)
+            nullify (calfun)
+            nullify (orig_calfun)
+        end do
     end do
 end do
 
@@ -94,8 +99,10 @@ character(len=PNLEN) :: probname
 character(len=PNLEN) :: probs(100)
 integer(IK) :: iprob
 integer(IK) :: ir
+integer(IK) :: isol
 integer(IK) :: m
 integer(IK) :: nprobs
+integer(IK) :: nsols
 integer(IK) :: nr
 procedure(FUNCON), pointer :: calcfc
 real(RP) :: Delta0
@@ -107,52 +114,55 @@ real(RP), allocatable :: x(:)
 type(problem_t) :: prob
 
 
+nsols = 3_IK
 nprobs = 3_IK
 probs(1:nprobs) = ['hexagon', 'hexagon', 'hexagon']
 nr = 3_IK
 
-do iprob = 1, nprobs
-    probname = probs(iprob)
-    do ir = 1, nr
-        print *, 'Constrained problem No.', iprob, '        Random run No.', ir
-        ! Construct the testing problem.
-        call construct(prob, probname)
+do isol = 1, nsols
+    do iprob = 1, nprobs
+        probname = probs(iprob)
+        do ir = 1, nr
+            print *, 'Solver No.', isol, '    Constrained problem No.', iprob, '     Random run No.', ir
+            ! Construct the testing problem.
+            call construct(prob, probname)
 
-        ! Read X0.
-        m = prob % m
-        call safealloc(x, prob % n) ! Not all compilers support automatic allocation yet, e.g., Absoft.
-        x = noisy(prob % x0)
+            ! Read X0.
+            m = prob % m
+            call safealloc(x, prob % n) ! Not all compilers support automatic allocation yet, e.g., Absoft.
+            x = noisy(prob % x0)
 
-        ! Read objective/constraints.
-        orig_calcfc => prob % calcfc
-        calcfc => noisy_calcfc  ! Impose noise to the test problem
+            ! Read objective/constraints.
+            orig_calcfc => prob % calcfc
+            calcfc => noisy_calcfc  ! Impose noise to the test problem
 
-        ! Read other data.
-        call safealloc(Aineq, int(size(prob % Aineq, 1), IK), int(size(prob % Aineq, 2), IK))
-        Aineq = prob % Aineq
-        call safealloc(bineq, int(size(prob % bineq), IK))
-        bineq = prob % bineq
-        Delta0 = prob % Delta0
+            ! Read other data.
+            call safealloc(Aineq, int(size(prob % Aineq, 1), IK), int(size(prob % Aineq, 2), IK))
+            Aineq = prob % Aineq
+            call safealloc(bineq, int(size(prob % bineq), IK))
+            bineq = prob % bineq
+            Delta0 = prob % Delta0
 
-        ! Call the solver.
-        !call solver_con(calcfc, x, f, constr, m, Aineq, bineq, Delta0)  ! sunf95 cannot handle this
-        call solver_con(noisy_calcfc, x, f, constr, m, Aineq, bineq, Delta0)
+            ! Call the solver.
+            !call solver_con(calcfc, x, f, constr, m, Aineq, bineq, Delta0)  ! sunf95 cannot handle this
+            call solver_con(noisy_calcfc, x, f, constr, m, Aineq, bineq, Delta0)
 
-        ! Try modifying PROB.
-        prob % x0 = x
+            ! Try modifying PROB.
+            prob % x0 = x
 
-        ! Try outputting PROB.
-        print *, 'Constrained problem solved with X = ', prob % x0
-        print *, ''
+            ! Try outputting PROB.
+            print *, 'Constrained problem solved with X = ', prob % x0
+            print *, ''
 
-        ! Clean up.
-        ! Destruct the testing problem.
-        call destruct(prob)
-        deallocate (x)
-        deallocate (Aineq)
-        deallocate (bineq)
-        nullify (calcfc)
-        nullify (orig_calcfc)
+            ! Clean up.
+            ! Destruct the testing problem.
+            call destruct(prob)
+            deallocate (x)
+            deallocate (Aineq)
+            deallocate (bineq)
+            nullify (calcfc)
+            nullify (orig_calcfc)
+        end do
     end do
 end do
 
