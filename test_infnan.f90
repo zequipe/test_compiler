b2d0f12469369f11fb0848c66c5e54a9d01513a2
zaikunzhang <zaikunzhang@gmail.com>
2022-05-22 17:33:13 +0800

    revise test_infnan

--- test_infnan.f90
+++ test_infnan.f90
@@ -1,7 +1,7 @@
 !        This is file : test_infnan
 ! Author= zaikunzhang
 ! Started at: 29.04.2022
-! Last Modified: Friday, April 29, 2022 PM12:05:23
+! Last Modified: Sunday, May 22, 2022 PM05:31:55
 
 program test_infnan
 
@@ -10,14 +10,16 @@ program test_infnan
 use, intrinsic :: iso_fortran_env, only : RP => REAL128
 use, intrinsic :: ieee_arithmetic, only : ieee_is_nan
 implicit none
-real(RP) :: l, u, d, y, z
+real(RP) :: l, u, d, x, y, z
 
 l = -huge(0.0_RP) / 4.0_RP
 u = huge(0.0_RP) / 4.0_RP
 d = 1.0E-002_RP
 y = l / d
 z = u / d
+x = y - y
 
+write (*, *) 'X = ', x, 'Expected NaN'
 write (*, *) 'Y = ', y, 'Expected -Inf'
 write (*, *) 'ABS(Y) = ', abs(y), 'Expected Inf'
 write (*, *) 'Z = ', z, 'Expected Inf'
@@ -26,6 +28,8 @@ write (*, *) 'Y < 0?', y < 0
 write (*, *) 'Z < 0?', z < 0
 write (*, *) 'Y is NaN?', ieee_is_nan(y)
 write (*, *) 'Z is NaN?', ieee_is_nan(z)
+write (*, *) 'Y < X?', y < x
+write (*, *) 'Z > X?', z > x
 
 if (ieee_is_nan(y) .and. (y < 0 .or. y > 0 .or. y == 0)) then
     write (*, *) 'Error: Y is both NaN and not NaN'
@@ -37,5 +41,14 @@ if (ieee_is_nan(z) .and. (z < 0 .or. z > 0 .or. z == 0)) then
     stop 2
 end if
 
+if (ieee_is_nan(x) .and. y < x) then
+    write (*, *) 'Error: -Inf < NaN'
+    stop 3
+end if
+
+if (ieee_is_nan(x) .and. z > x) then
+    write (*, *) 'Error: Inf > NaN'
+    stop 4
+end if
 
 end program test_infnan
