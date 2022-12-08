c7ce708f550e3b806f8365c595a40ee090c95c03
zaikunzhang <zaikunzhang@gmail.com>
2022-12-08 15:55:36 +0800

    add test_nan

--- test_nan.f90
+++ test_nan.f90
@@ -1,32 +1,24 @@
-!        This is file : test_nan
-! Author= zaikunzhang
-! Started at: 16.04.2022
-! Last Modified: Saturday, April 16, 2022 PM12:17:58
+!--------------------------------------------------------------------------------------------------!
+! $ uname -a && ifort --version && ifort test_nan.f90 && ./a.out
+! Linux zP 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux
+! ifort (IFORT) 2021.7.1 20221019
+! Copyright (C) 1985-2022 Intel Corporation.  All rights reserved.
+!
+! A =   6.9849113E+26  2.1425830E+27 -1.4108133E+27 -1.4853050E+26 -2.1667708E+27
+! B =        Infinity
+! A / B =             NaN            NaN            NaN            NaN   0.0000000E+00
+!--------------------------------------------------------------------------------------------------!
 
-program test_nan
-!use, intrinsic :: iso_fortran_env, only : RP => REAL32  ! Should be tested also
-!use, intrinsic :: iso_fortran_env, only : RP => REAL64  ! Should be tested also
-use, intrinsic :: iso_fortran_env, only : RP => REAL128
+program test
 implicit none
 
-real(RP) :: x(0), y
-logical :: yleh, ygeh, aygh, yisnan
+real, parameter :: a(5) = [6.9849113E+26, 2.1425830E+27, -1.4108133E+27, -1.4853050E+26, -2.1667708E+27]
+real :: b
 
-y = maxval(x)  ! Expected: -HUGE(X); or should it be -INF if the compiler supports INF?
-yleh = (y <= huge(y))  ! Expected: T
-ygeh = (y >= -huge(y))  ! Expected : T if Y == -HUGE(X), F if Y == -INF
-aygh = (abs(y) > huge(y))  ! Expected: F if Y == -HUGE(X), T if Y == -INF
-yisnan = (.not. (y <= huge(y) .and. y >= -huge(y))) .and. (.not. abs(y) > huge(y))  ! Expected: F
+b = 1.0 / tiny(1.0)**2
 
-write (*, *) 'Y =', y, ', Expected', -huge(x)
-write (*, *) 'YLEH = ', yleh, ', Expected', .true.
-write (*, *) 'YGEH = ', ygeh, ', Expected', .true.
-write (*, *) 'AYGH = ', aygh, ', Expected', .false.
-write (*, *) 'YISNAN = ', yisnan, ', Expected', .false.
+write (*, *) 'A = ', a
+write (*, *) 'B = ', b
+write (*, *) 'A / B = ', a / b
 
-if (yisnan) then
-    write (*, *) 'Error: Y is recognized as NaN while it is', y
-    stop 1
-end if
-
-end program test_nan
+end program test
