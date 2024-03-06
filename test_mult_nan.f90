4d91f91f73eddccf28afc658ce87972c0fc1208b
zaikunzhang <zaikunzhang@gmail.com>
2024-03-06 14:57:59 +0800

    revise test_mult_nan

--- test_mult_nan.f90
+++ test_mult_nan.f90
@@ -1,10 +1,21 @@
 !--------------------------------------------------------------------------------------------------!
+! ! On Ubuntu 22.04:
+!
 ! $ uname -a && nagfor test_mult_nan.f90 && ./a.out
 ! Linux 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
 ! NAG Fortran Compiler Release 7.1(Hanzomon) Build 7143
 ! [NAG Fortran Compiler normal termination]
 !  A is wrong!
 !  A =  NaN NaN
+! ERROR STOP: 1
+!
+! On macOS:
+!  % uname -a && nagfor test_mult_nan.f90&& ./a.out
+! Darwin 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:59 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6030 arm64
+! NAG Fortran Compiler Release 7.1(Hanzomon) Build 7144
+! [NAG Fortran Compiler normal termination]
+!  A =   0.0000  0.0000
+!  A is wrong!
 ! ERROR STOP: 1
 !--------------------------------------------------------------------------------------------------!
 program test_mult_nan
