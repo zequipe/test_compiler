26972d31cb981ebe4e31a817bf0a1bb984d0bfac
zaikunzhang <zaikunzhang@gmail.com>
2024-03-06 14:55:47 +0800

    revise test_mult_nan

--- test_mult_nan.f90
+++ test_mult_nan.f90
@@ -3,8 +3,8 @@
 ! Linux 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
 ! NAG Fortran Compiler Release 7.1(Hanzomon) Build 7143
 ! [NAG Fortran Compiler normal termination]
+!  A is wrong!
 !  A =  NaN NaN
-!  WRONG! A is NaN
 ! ERROR STOP: 1
 !--------------------------------------------------------------------------------------------------!
 program test_mult_nan
@@ -19,10 +19,9 @@ real(RP) :: A(2)
 A = 1.0_RP
 A = -1.0_RP * A
 
-write (*, *) 'A = ', A
-
-if (any(ieee_is_nan(A))) then
-    write (*, *) 'WRONG! A is NaN'
+if (any(ieee_is_nan(A)) .or. any(abs(A + 1.0_RP) > 10.0_RP * epsilon(1.0_RP))) then
+    write (*, *) 'A = ', A
+    write (*, *) 'A is wrong!'
     error stop 1
 end if
 
